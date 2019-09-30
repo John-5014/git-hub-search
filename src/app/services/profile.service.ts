f7c3db72d843b8ff879f8cee377e4672bbe1f7c2
John-5014 <kaiserjohn52@gmail.com>
2019-09-30 13:37:23 +0300

    add code for depencies and code for fetchig data from server with access token key;also include git hub username for dispaly of landing page

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -7,8 +7,39 @@ import { User } from '../user-class/user';
   providedIn: 'root'
 })
 export class ProfileService {
+  users: any;
+  user: User;
 
-    constructor() { }
+
+  private username: string;
+
+  private access_token = '2c900f56e6f75b8b13eb1311b8aa249da7ffc721';
+
+  constructor(private http: HttpClient) {
+    // tslint:disable-next-line: quotemark
+    console.log("service is ready");
+    this.username = 'john-5014';
+    this.access_token = '2c900f56e6f75b8b13eb1311b8aa249da7ffc721';
+
+   }
+   getProfileInfo() {
+     // tslint:disable-next-line: quotemark
+     return this.http.get("https://api.github.com/users/" + this.username + "?access_token=" +
+     this.access_token + '&access_token=' + this.access_token );
+   }
+
+   getProfileRepos() {
+    return this.http.get('https://api.github.com/users/' + this.username + '/repos?access_token=' +
+    // tslint:disable-next-line: quotemark
+    this.access_token + "/repos&access_token=" + this.access_token );
+  }
+  updateProfile(username: string) {
+    this.username = username;
+    console.log(username);
+
+  }
+
+   
 
 }
 
