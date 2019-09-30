fad914fe71990298f0a56f5b8fb2e7fef7af680f
John-5014 <kaiserjohn52@gmail.com>
2019-09-30 14:09:20 +0300

    add functionality of about section with list of repositories

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,8 +8,31 @@ import{ProfileService} from '../services/profile.service';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  profile:any;
+  repos:any;
+   username:any;
 
-  constructor() { }
+   constructor(public profileService:ProfileService) {
+    this.profileService.getProfileInfo().subscribe(profile => {
+      console.log(profile);
+      this.profile=profile;
+    });
+    this.profileService.getProfileRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos=repos;
+    });
+  }
+  findProfile(){
+    this.profileService.updateProfile(this.username);
+    this.profileService.getProfileInfo().subscribe (profile => {
+      console.log(profile);
+      this.profile=profile;
+    });
+    this.profileService.getProfileRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos=repos;
+    });
+  }
 
 
   ngOnInit() {
