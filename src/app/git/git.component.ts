29671fbf1634652634ba7b167afe7c9fba73ae8e
John-5014 <kaiserjohn52@gmail.com>
2019-09-30 13:26:43 +0300

    import neccessary path definitions for effective functionality and getting info on from profile and user

---
+++ src/app/git/git.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import {User} from '../user-class/user';
+import { HttpClient } from '@angular/common/http';
+import { ProfileService } from '../services/profile.service';
+// import {Repository} '../repository-class/repository';
+
+@Component({
+  selector: 'app-git',
+  templateUrl: './git.component.html',
+  providers: [ProfileService],
+  styleUrls: ['./git.component.css']
+})
+export class GitComponent implements OnInit {
+
+ public username = 'john-5014';
+ public  getProfileInfo: string;
+ user: User;
+//  findUser(username){
+//    this. getProfileInfo='';
+//    this.username=username;
+//    this.ngOnInit();
+//  }
+
+
+
+  constructor(public userRequest: ProfileService, public repoRequest: ProfileService) { }
+
+  ngOnInit() {
+    // this.userRequest.users(this.username);
+    // this.user=this.userRequest.user;
+    // this.repoRequest.use
+
+   }
+
+}
