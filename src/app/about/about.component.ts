33dafe71f537701589b4ccbfe0a12af10cfe0746
John-5014 <kaiserjohn52@gmail.com>
2019-09-30 14:06:07 +0300

    import ProfileService and path for dependency

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import{ProfileService} from '../services/profile.service';
+
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+
+  ngOnInit() {
+  }
+
+}
