6074125f88ff1d86a8f0f8bc52801dc96ae9e70c
John-5014 <kaiserjohn52@gmail.com>
2019-09-30 12:02:57 +0300

    add functionality of profile.component.ts after imports

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -11,12 +11,35 @@ import { ProfileService } from '../../services/profile.service';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  profile: any;
+  repos: any;
+   username: any;
 
 
 
-  constructor() { }
+   constructor(public profileService: ProfileService) {
+    this.profileService.getProfileInfo().subscribe(profile => {
+      console.log(profile);
+      this.profile = profile;
+    });
+    this.profileService.getProfileRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+    });
+  }
+  findProfile() {
+    this.profileService.updateProfile(this.username);
+    this.profileService.getProfileInfo().subscribe(profile => {
+      console.log(profile);
+      this.profile = profile;
+    });
+    this.profileService.getProfileRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+    });
+  }
 
   ngOnInit() {
-    
+
    }
 }
